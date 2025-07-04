35f2c770a95e59e24ac49983e851f73d5d1e7ae7
bgsptr <agunggusti95@gmail.com>
2025-07-04 19:26:55 +0800

    feat(swagger): setup and initiate swagger api

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 import { DomainExceptionsFilter } from './shared/filters/domain-exception.filter';
 import { LoggerService } from './infrastructure/logger/logger.service';
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 import { ZodValidationFilter } from './shared/filters/zod.filter';
 import * as express from 'express';
 import { join } from 'path';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 // import * as bodyParser from 'body-parser';
 
 dotenv.config();
@@ -24,7 +25,7 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
-  })
+  });
   // app.useGlobalPipes(
   //   new ValidationPipe({
   //     transform: true,
@@ -35,8 +36,19 @@ async function bootstrap() {
   const loggerService = await app.resolve(LoggerService);
   app.useGlobalFilters(
     new ZodValidationFilter(),
-    new DomainExceptionsFilter(loggerService)
+    new DomainExceptionsFilter(loggerService),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
